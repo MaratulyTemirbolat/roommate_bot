6888891970a8ed8eacb20789368eabb6c6c5fd01
Temirbolat <snake2009003@mail.ru>
2023-08-15 23:26:47 +0600

    File with all basic hadlers for routing (start, etc.)

--- core/handlers/basics.py
+++ core/handlers/basics.py
@@ -3,27 +3,39 @@ from typing import (
     Tuple,
     Dict,
     Any,
+    Optional,
 )
+
+# Third party
+from aioredis import Redis
 
 # Aiogram
 from aiogram import Bot
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+# Project
+from core.keyboards.reply.reply import get_main_reply_keyboard
 
 
 async def get_start(
     message: Message,
     bot: Bot,
+    state: FSMContext,
+    redis_cli: Redis,
     *args: Tuple[Any],
     **kwargs: Dict[Any, Any]
 ) -> None:
+    """Handle START command function."""
+    is_registered: Optional[Any] = await redis_cli.get(
+        name=message.from_user.id
+    )
+    await state.clear()
     await bot.send_message(
         chat_id=message.from_user.id,
-        text=f"<b>Привет {message.from_user.first_name}. Рад тебя видеть</b>"
-    )
-    await message.answer(
-        text=f"<s>Привет {message.from_user.first_name}. Рад тебя видеть</s>"
-    )
-    await message.reply(
-        text=f"<tg-spoiler>Привет\
-{message.from_user.first_name}. Рад тебя видеть</tg-spoiler>"
+        text=f"Привет <b>{message.from_user.first_name}</b>. "
+        "Рад тебя видеть в нашем чате\r\n",
+        reply_markup=get_main_reply_keyboard(
+            is_registered_user=is_registered
+        )
     )
